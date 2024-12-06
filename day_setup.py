8487f95f5ea5bea9d33d79609283a3672207fbf7
Alberto Minetti <alberto.minetti@gmail.com>
2024-12-06 17:29:51 +0100

    create two files during the setup for the day, part1 and part2

--- day_setup.py
+++ day_setup.py
@@ -4,29 +4,33 @@ import requests
 year = datetime.datetime.now().year
 day = datetime.datetime.now().day
 
-for day in range(1,6):
-    input_path = "day{:02d}/input.txt".format(day)
-    with open("cookies.txt", "r") as file:
-        for line in file:
-            match = re.search(r'session=([a-f0-9]+)$', line)
-            if match:
-                session_value = match.group(1)
-                break
+input_path = "day{:02d}/input.txt".format(day)
+with open("cookies.txt", "r") as file:
+    for line in file:
+        match = re.search(r'session=([a-f0-9]+)$', line)
+        if match:
+            session_value = match.group(1)
+            break
 
-    url = f"https://adventofcode.com/{year}/day/{day}/input"
-    headers = {"cookie": f"session={session_value}",
-               "User-Agent": "github.com/albertominetti/adventofcode2024-python"}
+url = f"https://adventofcode.com/{year}/day/{day}/input"
+headers = {"cookie": f"session={session_value}",
+           "User-Agent": "github.com/albertominetti/adventofcode2024-python"}
 
-    r = requests.get(url, headers=headers)
-    content = r.text
-    os.makedirs(os.path.dirname(input_path), exist_ok=True)
-    with open(input_path, 'w+') as fp:
-        fp.write(content)
+r = requests.get(url, headers=headers)
+content = r.text
+os.makedirs(os.path.dirname(input_path), exist_ok=True)
+with open(input_path, 'w+') as fp:
+    fp.write(content)
 
-    code_path = "day{:02d}/main.py".format(day)
-    with open(code_path, 'w+') as fp:
-        fp.write("")
+test_path = "day{:02d}/input_test.txt".format(day)
+open(test_path, 'a').close()
 
-    test_path = "day{:02d}/input_test.txt".format(day)
-    open(test_path, 'a').close()
+code_path = "day{:02d}/main{:02d}p1.py".format(day, day)
+with open(code_path, 'w+') as fp:
+    fp.write("open(\"input_test.txt\").read().splitlines()")
+
+code_path = "day{:02d}/main{:02d}p2.py".format(day, day)
+with open(code_path, 'w+') as fp:
+    fp.write("open(\"input_test.txt\").read().splitlines()")
+
 
