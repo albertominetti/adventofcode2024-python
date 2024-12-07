2c870b5e27c1721da1b05d799c0e8bf2d9a88e6f
Alberto Minetti <alberto.minetti@gmail.com>
2024-12-07 16:33:59 +0100

    day02 (part 1)
    Took 15 minutes

---
+++ day02/main02p1.py
@@ -0,0 +1,11 @@
+lists = [list(map(int, l.split())) for l in open("input_test.txt").read().splitlines()]
+
+print(lists)
+
+count = 0
+for l in lists:
+    differences = [a - b for a,b in zip(l,l[1:])]
+    valid = all(0 < d <= 3 for d in differences) or all(-3 <= d < 0 for d in differences)
+    if valid:
+        count = count + 1
+print(count)
