8d8c2d456f6f61b8d94edcf31e47febd74763559
Alberto Minetti <alberto.minetti@gmail.com>
2024-12-13 16:59:01 +0100

    day13 (part 1)
    Took 35 minutes

---
+++ day13/main13p1.py
@@ -0,0 +1,17 @@
+import re
+
+input = open("input.txt").read()
+
+total = 0
+for block in input.split("\n\n"):
+    ax, ay, bx, by, tx, ty = map(int, re.findall(r"(\d+)", block))
+    if (by * ax - bx * ay) == 0:
+        print("error: division by Zero")
+
+    ac = (tx * by - ty * bx) / (by * ax - bx * ay)
+    bc = (ty * ax - tx * ay) / (ax * by - bx * ay)
+    if int(ac) == ac and int(bc) == bc and ac <= 100 and bc <= 100:
+        print(f"For {ax, ay, bx, by, tx, ty} a solution is with Bx{bc} and Ax{ac}")
+        total += bc * 1 + ac * 3
+
+print(total)
